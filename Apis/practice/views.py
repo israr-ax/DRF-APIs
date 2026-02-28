74e4e0e94e9697cd34de7a5ddf885942bb35bf6c
israr-ax <israrmemon211@gmail.com>
2026-02-28 16:58:11 +0500

    Login & logout API

--- Apis/practice/views.py
+++ Apis/practice/views.py
@@ -20,3 +20,25 @@ def get_token_for_user(user):
     refresh=RefreshToken.for_user(user)
     return { "access" : str(refresh.access_token),
             "refresh": str(refresh),}
+    
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                {"success": False, "errors": serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST)
+        user = serializer.save()
+        tokens = get_token_for_user(user)
+        return Response ({
+            
+            "success": True,
+            "message": "Account created successfully.",
+            "user": {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+            },
+            "tokens": tokens,
+        }, status=status.HTTP_201_CREATED)
