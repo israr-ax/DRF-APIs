ab7f48120be667e2dd9185f294f9db72e2e7fdab
israr-ax <israrmemon211@gmail.com>
2026-02-28 17:05:42 +0500

    Login & logut Api

---
+++ Apis/practice/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import (
+    RegisterView,
+    LoginView,
+    LogoutView,
+    ProfileView,
+    UserListView,
+)
+
+urlpatterns = [
+    # Auth
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # User
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('users/', UserListView.as_view(), name='user_list'),
+]
